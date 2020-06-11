50dc02034f2581babd4d26ef54796047f3556175
Jacky <jackycct@consultant.com>
2020-06-11 23:48:41 +0800

    Spring Boot Initial

---
+++ src/main/java/com/example/wechatwork/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.example.wechat.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.controller")
+public class WebConfig {
+    //Other configurations
+}
---
+++ src/main/java/com/example/wechatwork/controller/EventCallbackController.java
@@ -0,0 +1,19 @@
+package com.example.wechat.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EventCallbackController {
+    @PostMapping
+    public ResponseEntity<?> callback(@RequestParam("msg_signature") String message,
+                                      @RequestParam("nonce") String nonce){
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+}
