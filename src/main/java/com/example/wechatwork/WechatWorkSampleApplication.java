1251c4af806c391c1bb4a0bbcc2f6929d49230a2
Jacky <jackycct@consultant.com>
2020-06-16 23:48:50 +0800

    Added Logger

--- src/main/java/com/example/wechatwork/WechatWorkSampleApplication.java
+++ src/main/java/com/example/wechatwork/WechatWorkSampleApplication.java
@@ -6,21 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+import org.slf4j.*;
 
 @SpringBootApplication
 public class WechatWorkSampleApplication implements CommandLineRunner {
+	private static final Logger LOGGER = LoggerFactory.getLogger(WechatWorkSampleApplication.class);
+
 	@Autowired
 	private WechatWorkGateway gw;
 
+	public static void main(String[] args) { SpringApplication.run(WechatWorkSampleApplication.class, args); }
 
-	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(WechatWorkSampleApplication.class);
-		app.run();
-		// SpringApplication.run(WechatWorkSampleApplication.class, args);
-	}
-
+	@Profile("local")
 	public void run(String... args) throws Exception {
 		GetTokenResponse res = gw.getAccessToken();
-		System.out.println(res.getAccess_token());
+		LOGGER.debug("Access Token: " + res.getAccess_token());
 	}
 }
