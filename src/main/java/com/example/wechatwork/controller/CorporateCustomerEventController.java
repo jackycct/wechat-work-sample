4109b65d907893f204b64377644d5129045b1afb
Jacky <jackycct@consultant.com>
2020-06-16 23:49:55 +0800

    Send welcome message to people who joined the chat room. To be debugged

--- src/main/java/com/example/wechatwork/controller/CorporateCustomerEventController.java
+++ src/main/java/com/example/wechatwork/controller/CorporateCustomerEventController.java
@@ -1,27 +1,31 @@
 package com.example.wechatwork.controller;
 
 import com.example.wechatwork.config.WechatWorkConfig;
+import com.example.wechatwork.gateway.WechatWorkGateway;
+import com.example.wechatwork.model.GetTokenResponse;
 import lombok.val;
 import me.chanjar.weixin.common.util.XmlUtils;
 import me.chanjar.weixin.common.util.crypto.WxCryptUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
 
 @Controller
 public class CorporateCustomerEventController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CorporateCustomerEventController.class);
+
     @Autowired
     private WechatWorkConfig wechatWorkConfig;
+    @Autowired
+    private WechatWorkGateway gw;
 
     @GetMapping("/ping")
     public ResponseEntity<?> ping() {
@@ -64,7 +68,16 @@ public class CorporateCustomerEventController {
         System.out.println("********** New Message received **************");
         msgContent.forEach((k, v) -> System.out.println(k + " : " + v));
 
-        return new ResponseEntity<>("", HttpStatus.OK);
+        if ("enter_agent".equals(msgContent.get("Event"))) {
+            GetTokenResponse res = gw.getAccessToken();
+            LOGGER.debug("Access Token: " + res.getAccess_token());
+            String results = gw.send(res.getAccess_token(), (String) msgContent.get("FromUserName"), (String) msgContent.get("AgentID"), (String) msgContent.get("welcome!!!"));
+            System.out.println(results);
+
+            // TODO Fix this error
+            // {"errcode":40008,"errmsg":"invalid message type, hint: [1592319353_77_4e699f9e415228292dde839554c6191c], from ip: 220.246.156.20, more info at https://open.work.weixin.qq.com/devtool/query?e=40008"}
+        }
+        return new ResponseEntity<>("reply", HttpStatus.OK);
     }
 
     private void writeStringToFile(String eventString) {
--- src/main/java/com/example/wechatwork/gateway/WechatWorkGateway.java
+++ src/main/java/com/example/wechatwork/gateway/WechatWorkGateway.java
@@ -2,11 +2,15 @@ package com.example.wechatwork.gateway;
 
 import com.example.wechatwork.config.WechatWorkConfig;
 import com.example.wechatwork.model.GetTokenResponse;
+import lombok.val;
+import me.chanjar.weixin.common.api.WxConsts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.HashMap;
 
 @Component
 public class WechatWorkGateway {
@@ -47,6 +51,33 @@ public class WechatWorkGateway {
                         .path("/cgi-bin/getcallbackip")
                         .queryParam("access_token", accessToken)
                         .build())
+                .retrieve();
+
+        return response.bodyToMono(String.class).block();
+    }
+
+    public String send(String accessToken, String touser, String agentid, String message) {
+        WebClient client = WebClient
+                .builder()
+                .baseUrl("https://qyapi.weixin.qq.com/")
+                .defaultCookie("cookieKey", "cookieValue")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        val body = new HashMap<String, Object>();
+        body.put("ToUser", touser);
+        body.put("MsgType", "text");
+        body.put("AgentID", agentid);
+        val content = new HashMap<String, String>();
+        content.put("Content", message);
+        body.put("text", content);
+
+        WebClient.ResponseSpec response;
+        response = client.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/cgi-bin/message/send")
+                        .queryParam("access_token", accessToken)
+                        .build(body))
                 .retrieve();
 
         return response.bodyToMono(String.class).block();
